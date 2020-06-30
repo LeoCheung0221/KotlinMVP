7b5f7e392e5edc73a4dd67b4ed827a16ae45c2e9
leocheung <leocheung4ever@163.com>
2020-06-30 08:17:33 +0800

    init super kt-mvp framework

---
+++ app/src/main/java/com/tufusi/superktpro/modules/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.tufusi.superktpro.modules.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tufusi.superktpro.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/com/tufusi/superktpro/net/ApiClient.kt
@@ -0,0 +1,14 @@
+package com.tufusi.superktpro.net
+
+/**
+ * File: ApiClient.java
+ * Author: leocheung
+ * Version: V100R001C01
+ * Create: 2020/6/30 7:10 AM
+ * Description:
+ *
+ * Changes (from 2020/6/30)
+ * -----------------------------------------------------------------
+ * 2020/6/30 : Create ApiClient.java (leocheung);
+ * -----------------------------------------------------------------
+ */
